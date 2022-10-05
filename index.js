2c647cf55d0b47bef526380c1d9894a8cfb7283d
ekukwa <easmsm@gmail.com>
2022-10-05 15:01:53 -0400

    added pseudocode to index.js for inquirer prompts

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+
+const generateHTML = require('./src/generateHTML');
+const teamArray = [];
+
+//inquirer prompts - would love to add more logic here in the future to combine these prompts with conditional prompts (add the office number prompt only if the role is manager)
+
+    //start with base/default employee
+
+    //engineer prompts
+
+    //intern prompts
+
+    //manager prompts
+
+//function to add the employee
+
+//feed these prompts into the cards
