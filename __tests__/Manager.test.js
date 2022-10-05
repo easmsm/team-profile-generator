cc3cd12fee759b7d4c8e21ae8c10f960ad3064fa
ekukwa <easmsm@gmail.com>
2022-10-05 13:27:15 -0400

    all tests now pass

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,7 +1,7 @@
 const Manager = require('../lib/Manager');
 
 test('creates an Manager object', () => {
-    const manager = new Manager('Emily', 1, 'test@test.com', 3);
+    const manager = new Manager('Emily', 2, 'test@test.com', 3);
     
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
